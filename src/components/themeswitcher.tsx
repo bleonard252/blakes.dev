9bdf9b641d82978021c0cf30ddff81106c1e09b8
Blake <blake@1024256.xyz>
2022-05-02 23:37:48 -0400

    Variable based theme system
    I don't have to declare classes for literally everything anymore :tada:
    
    Signed-off-by: Blake <blake@1024256.xyz>

--- src/components/themeswitcher.tsx
+++ src/components/themeswitcher.tsx
@@ -1,12 +1,16 @@
 import { X_Apply_Theme, X_Current_Theme } from "../scripts/applytheme";
+import { useState } from 'preact/hooks';
 
 export default function (props) {
+  const [themeValue, updateThemeValue] = useState(localStorage.theme);
   return <dialog id="themeSwitcher" class="p-10 m-1 rounded-lg shadow-xl max-w-sm lg:max-w-md xl:max-w-lg max-h-[75vh] flex flex-wrap bg-scheme-2">
     <ThemeTile 
       bgClass="bg-slate-50 text-black"
       primaryClass="bg-blue-700 text-white hover:bg-blue-500"
       cardClass="bg-slate-100"
       theme="arc-light"
+      active={themeValue == "arc-light"}
+      updateThemeValue={updateThemeValue}
       name="Arc Light"
     />
     <ThemeTile 
@@ -14,6 +18,8 @@ export default function (props) {
       primaryClass="bg-blue-700 text-white hover:bg-blue-500"
       cardClass="bg-slate-800"
       theme="arc-dark"
+      active={themeValue == "arc-dark"}
+      updateThemeValue={updateThemeValue}
       name="Arc Dark"
     />
     <ThemeTile 
@@ -21,6 +27,8 @@ export default function (props) {
       primaryClass="bg-green-600 text-white hover:bg-green-400"
       cardClass="bg-mint-100"
       theme="mint-light"
+      active={themeValue == "mint-light"}
+      updateThemeValue={updateThemeValue}
       name="Mint Light"
     />
     <ThemeTile 
@@ -28,6 +36,8 @@ export default function (props) {
       primaryClass="bg-green-600 text-white hover:bg-green-400"
       cardClass="bg-mint-800"
       theme="mint-dark"
+      active={themeValue == "mint-dark"}
+      updateThemeValue={updateThemeValue}
       name="Mint Dark"
     />
     <ThemeTile 
@@ -35,6 +45,8 @@ export default function (props) {
       primaryClass="bg-orange-600 text-white hover:bg-orange-400"
       cardClass="bg-zinc-100"
       theme="unity-light"
+      active={themeValue == "unity-light"}
+      updateThemeValue={updateThemeValue}
       name="Unity Light"
     />
     <ThemeTile 
@@ -42,9 +54,11 @@ export default function (props) {
       primaryClass="bg-orange-600 text-white hover:bg-orange-400"
       cardClass="bg-zinc-800"
       theme="unity-dark"
+      active={themeValue == "unity-dark"}
+      updateThemeValue={updateThemeValue}
       name="Unity Dark"
     />
-    <div class="w-full">
+    <div class="w-full mb-0 pb-0">
       <form method="dialog">
         <button type="submit" class="p-2 m-2 text-primary-3 inline-block bg-opacity-0 hover:bg-opacity-25 bg-primary-1 rounded-md transition-colors">
           Exit
@@ -54,9 +68,9 @@ export default function (props) {
   </dialog>;
 }
 
-function ThemeTile({ bgClass = "bg-white", primaryClass="bg-red-500", theme="none", name="Theme", cardClass="bg-gray-500" }) {
-  return <div class={bgClass+" rounded-md "+(X_Current_Theme() === theme ? "border-primary-3" : "border-scheme-3")+" border-2 m-2 w-48 h-36 relative hover:shadow-lg active:shadow-2xl transition-shadow"}
-    onClick={() => {localStorage.theme = theme; X_Apply_Theme();}}>
+function ThemeTile({ active = false, updateThemeValue = (value: string) => {}, bgClass = "bg-white", primaryClass="bg-red-500", theme="none", name="Theme", cardClass="bg-gray-500" }) {
+  return <div class={bgClass+" rounded-md "+(active ? "border-primary-3" : "border-scheme-3")+" border-2 m-2 w-48 h-36 relative hover:shadow-lg active:shadow-2xl transition-shadow"}
+    onClick={() => {localStorage.theme = theme; X_Apply_Theme(); updateThemeValue(theme);}}>
     <div class={cardClass+" rounded-md absolute top-4 left-4 p-2 select-none"}>{name}</div>
     <button class={primaryClass+" rounded-md absolute right-4 bottom-4 p-4 select-none"}>Primary</button>
   </div>
