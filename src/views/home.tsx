ac71daff8fe1468aa3cef4015b0beb3116bea371
Blake <blake@1024256.xyz>
2022-05-09 12:18:02 -0400

    support CWs in statuses
    Signed-off-by: Blake <blake@1024256.xyz>

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -137,6 +137,37 @@ class View extends Component {
                 <Icon icon="carbon:launch" />
               </a>
             </div>
+            <dialog id={"sd"+status.id} class="p-4 m-1 rounded-lg shadow-xl max-w-[90vw] lg:max-w-md xl:max-w-lg w-full max-h-[75vh] flex flex-col bg-scheme-2 text-onscheme-2">
+              {status.reblog ? <div class="flex flex-row items-center p-4 pb-0">
+                <img src={status.account.avatar_static} class="h-4 rounded-full mr-2" />
+                <span>
+                  <strong>{status.account.display_name ?? status.account.username}</strong> boosted &bull; <span 
+                  title={tago.format(new Date(status.created_at), 'round-minute')+': '+(new Date(status.created_at)).toLocaleString()}>
+                    {tago.format(new Date(status.created_at), 'twitter-minute-now')}
+                  </span>
+                </span>
+              </div> : ``}
+              <this.Status status={status.reblog || status} showFull={true} />
+              <div class="X-Action-Row mt-auto flex flex-row">
+                <a href={"https://indieweb.social/interact/"+(status.reblog ?? status).id+"?type=reply"} aria-label="Reply" target="_blank" class="p-2 m-2 text-gray-500 inline-block hover:bg-opacity-25 hover:bg-blue-500 hover:text-blue-500 rounded-full transition-colors has-tooltip">
+                  <div class="tooltip rounded-md shadow-lg p-1 bg-scheme-3 mt-8 ml-2 -translate-x-[50%] text-onscheme-3" aria-hidden>Reply</div>
+                  <Icon icon="carbon:reply" />
+                </a>
+                <a href={"https://indieweb.social/interact/"+(status.reblog ?? status).id+"?type=boost"} aria-label="Boost" target="_blank" class="p-2 m-2 text-gray-500 inline-block hover:bg-opacity-25 hover:bg-green-500 hover:text-green-500 rounded-full transition-colors has-tooltip">
+                  <div class="tooltip rounded-md shadow-lg p-1 bg-scheme-3 mt-8 ml-2 -translate-x-[50%] text-onscheme-3" aria-hidden>Boost</div>
+                  <Icon icon="feather:repeat" />
+                </a>
+                <a href={"https://indieweb.social/interact/"+(status.reblog ?? status).id+"?type=favourite"} aria-label="Favorite" target="_blank" class="p-2 m-2 text-gray-500 inline-block hover:bg-opacity-25 hover:bg-red-500 hover:text-red-500 rounded-full transition-colors has-tooltip">
+                  <div class="tooltip rounded-md shadow-lg p-1 bg-scheme-3 mt-8 ml-2 -translate-x-[50%] text-onscheme-3" aria-hidden>Favorite</div>
+                  <Icon icon="feather:heart" />
+                </a>
+                <span class="flex-grow" />
+                <a href={status.url} aria-label="Read More" class="p-2 m-2 text-gray-500 inline-block hover:bg-scheme-3 hover:text-onscheme-3 rounded-full transition-colors has-tooltip">
+                  <div class="tooltip rounded-md shadow-lg p-1 bg-scheme-3 mt-8 ml-2 -translate-x-[100%] text-onscheme-3" aria-hidden>Read&nbsp;More</div>
+                  <Icon icon="carbon:launch" />
+                </a>
+              </div>
+            </dialog>
           </div>)}
         </div>
       </div>
@@ -144,26 +175,37 @@ class View extends Component {
     </Fragment>;
   }
 
-  Status({status}) {
+  Status({status, showFull = false}) {
     return <>
       <a class="group" href={status.account.url}>
         <div class="flex flex-row items-center p-4">
-          <img src={status.account.avatar_static} class="h-12 rounded-full mr-2" />
+          <img src={status.account.avatar_static} class="h-12 rounded-full mr-2" aria-hidden />
           <span>
-            <span class="text-primary-3 group-hover:underline">{status.account.display_name ?? status.account.username}</span><br />
+            <span class="text-primary-3 group-hover:underline">{status.account.display_name ?? status.account.username}<span class="hidden"> says</span></span><br />
             <span class="text-sm opacity-70">
-              @{status.account.acct.includes("@") ? status.account.acct : status.account.acct+"@indieweb.social"} &bull; <span 
-              title={tago.format(new Date(status.created_at), 'round-minute')+': '+(new Date(status.created_at)).toLocaleString()}>
+              <span aria-hidden>@{status.account.acct.includes("@") ? status.account.acct : status.account.acct+"@indieweb.social"} &bull; </span><span 
+              title={tago.format(new Date(status.created_at), 'round-minute')+': '+(new Date(status.created_at)).toLocaleString()}
+              aria-label={tago.format(new Date(status.created_at), 'twitter-minute-now')}>
                 {tago.format(new Date(status.created_at), 'twitter-minute-now')}
               </span>
             </span>
           </span>
         </div>
       </a>
+      {showFull ? <div class="flex-grow text-right absolute top-4 right-4"><form method="dialog">
+        <button type="submit" aria-label="Close dialog" target="_blank" class="p-2 m-2 text-gray-500 inline-block hover:bg-opacity-25 hover:bg-primary-3 hover:text-primary-3 rounded-full transition-colors has-tooltip">
+          <div class="tooltip rounded-md shadow-lg p-1 bg-scheme-3 mt-8 ml-2 -translate-x-[50%] text-onscheme-3" aria-hidden>Close dialog</div>
+          <Icon icon="feather:x" />
+        </button>
+      </form></div> : ``}
       {status.in_reply_to_account_id && status.in_reply_to_id ? <a href={"https://indieweb.social/web/statuses/"+status.in_reply_to_id} class="ml-4 mr-4 mb-2 text-sm italic opacity-70 hover:opacity-100 hover:underline">Replying to a post</a> : `` }
+      {status.spoiler_text && showFull ? <div class="ml-4 mr-4 mb-4 X-Arb">{status.spoiler_text}</div> : ``}
       <div class="ml-4 mr-4 X-Arb">
-        <SanitizedHTML html={applyShortcodes(status.content, status.emojis || [])} allowedTags={tags} allowedAttributes={attributes} allowedClasses={classes} />
+        {(!showFull && status.spoiler_text) || <SanitizedHTML html={applyShortcodes(status.content, status.emojis || [])} allowedTags={tags} allowedAttributes={attributes} allowedClasses={classes} />}
       </div>
+      {status.spoiler_text && !showFull ? <div class="ml-4 mr-4 text-primary-3 hover:underline">
+        <button onClick={() => {(document.getElementById("sd"+status.id) as any).showModal()}}><strong>Read behind content warning <InlineIcon icon="mdi:chevron-double-right" className="inline w-4 h-4" /></strong></button>
+      </div> : ``}
       {status.media_attachments ? <div class="m-4 mt-2 mb-0 flex flex-row">
         {status.media_attachments.map(att => att.type == "image" ? <a href={att.remote_url || att.url}>
           <img class="rounded-md max-h-[50vh]" src={att.preview_url} alt={att.description} title={att.description} />
