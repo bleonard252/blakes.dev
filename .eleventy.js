2fbd6913d4c63f0cf673f5af88ad4ff45c162e60
Blake Leonard <me@blakes.dev>
2023-04-07 15:38:25 -0400

    Optimize the existing bundle even further
    Signed-off-by: Blake Leonard <me@blakes.dev>

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,3 @@
-const mdxPlugin = require("@jamshop/eleventy-plugin-mdx");
-const preactRender = require('preact-render-to-string');
 const webcPlugin = require("@11ty/eleventy-plugin-webc");
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
@@ -8,19 +6,9 @@ module.exports = function eleventy(config) {
   config.addPassthroughCopy({'static': '/'});
   config.addPassthroughCopy({'node_modules/@11ty/is-land/is-land.js': '/scripts/vendor/is-land.js'});
   config.addPassthroughCopy({'node_modules/iconify-icon/dist/iconify-icon.min.js': '/scripts/vendor/iconify-icon.js'});
-  config.addPassthroughCopy({
-    // keep this up to date with "dependencies" in package.json and import map in layout.webc
-    'node_modules/@11ty/is-land/': '/scripts/vendor/node_modules/@11ty/is-land/',
-    'node_modules/htm/': '/scripts/vendor/node_modules/htm/',
-    'node_modules/iconify-icon/': '/scripts/vendor/node_modules/iconify-icon/',
-    'node_modules/isomorphic-dompurify/': '/scripts/vendor/node_modules/isomorphic-dompurify/',
-    'node_modules/javascript-time-ago/': '/scripts/vendor/node_modules/javascript-time-ago/',
-    'node_modules/preact/': '/scripts/vendor/node_modules/preact/',
-    'node_modules/@preact/compat/': '/scripts/vendor/node_modules/@preact/compat/',
-  });
   config.setUseGitIgnore(false);
   config.watchIgnores.add("src/_includes/styles/theme/*.yaml"); // this doesn't update anything unless you run `pnpm build:themes`
-  config.addPlugin(mdxPlugin);
+  //config.addPlugin(mdxPlugin);
   config.addPlugin(webcPlugin, {
     components: "src/_includes/components/{blakes-,**/blakes-,theme-switcher/}*.webc",
   });
@@ -35,7 +23,7 @@ module.exports = function eleventy(config) {
         copyPublicDir: true,
         rollupOptions: {
           output: {
-            preserveModules: true,
+            //preserveModules: true,
             // assetFileNames({ name }) {
             //   return name?.replace(/^src\//, '') ?? '';
             // }
