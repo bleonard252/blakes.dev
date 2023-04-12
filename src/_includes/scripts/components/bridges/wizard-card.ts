351edecc14d84e866f89f2adb51165889a0f01a6
Blake Leonard <me@blakes.dev>
2023-04-11 22:54:47 -0400

    Bridge Wizard: "Fediverse" on the AP card
    Signed-off-by: Blake Leonard <me@blakes.dev>

--- src/_includes/scripts/components/bridges/wizard-card.ts
+++ src/_includes/scripts/components/bridges/wizard-card.ts
@@ -20,13 +20,13 @@ export class WizardCard extends Component<h.JSX.HTMLAttributes & {
   /** Whether the platform is used for social or chat, if relevant.
    * This is typically used on the second page of the wizard with XMPP,
    * to distinguish between social (Libervia/Movim) and chat (Conversations) bridges.
-   * 
+   *
    * Social platforms where DMs are also bridged are still considered social.
    */
   category?: 'social' | 'chat' | null,
 }> {
   render({platform, category, ...props}) {
-    if (platform == 'activitypub') return html`<a class="X-BridgeWizardCard bg-amber-300 dark:bg-yellow-700 hover:bg-amber-400 dark:hover:bg-amber-600" ...${props}><h3>ActivityPub</h3>Mastodon, Misskey, Pixelfed, Peertube...</a>`;
+    if (platform == 'activitypub') return html`<a class="X-BridgeWizardCard bg-amber-300 dark:bg-yellow-700 hover:bg-amber-400 dark:hover:bg-amber-600" ...${props}><h3>ActivityPub</h3>Mastodon, Misskey, Pixelfed, Pleroma, "the Fediverse"...</a>`;
     else if (platform == 'nostr') return html`<a class="X-BridgeWizardCard bg-purple-300 dark:bg-purple-900 hover:bg-purple-400 dark:hover:bg-purple-800" ...${props}><h3>Nostr</h3></a>`;
     else if (platform == 'atproto') return html`<a class="X-BridgeWizardCard bg-sky-300 dark:bg-sky-900 hover:bg-sky-400 dark:hover:bg-sky-800" ...${props}><h3>AT Protocol</h3>Bluesky</a>`;
     else if (platform == 'matrix') return html`<a class="X-BridgeWizardCard bg-green-300 dark:bg-green-900 hover:bg-green-400 dark:hover:bg-green-800" ...${props}><h3>Matrix</h3>Element</a>`;
