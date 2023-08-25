ab86bf1ce6afe6e2ee2d1c457deef187bf9c688e
Blake Leonard <me@blakes.dev>
2023-08-24 20:24:28 -0400

    Switch references to infosec.town
    Signed-off-by: Blake Leonard <me@blakes.dev>

--- src/_includes/scripts/components/home-profile.ts
+++ src/_includes/scripts/components/home-profile.ts
@@ -15,14 +15,17 @@ export default function ({ accountResult }: ({accountResult: Partial<Entity.Acco
     ${/*a href="https://codeberg.org/bleonard252" class="float-right -mt-[36px] mb-0 p-2 bg-primary-3 inline-block hover:bg-primary-5 text-white rounded-md transition-colors">
       <InlineIcon icon="simple-icons:github" className="lg:inline" /><span class="hidden lg:inline"> Follow</span>
     </a>*/``}
-    <blakes-button primary filled href="https://fosstodon.org/users/blake/remote_follow" class="float-right -mt-[36px] mb-0">
+    <blakes-button primary filled href="https://infosec.town/@blake" class="float-right -mt-[36px] mb-0">
       <iconify-icon icon="simple-icons:mastodon" class="inline-block align-[-.2em]" aria-hidden /><span class="hidden lg:inline"> Follow</span>
     </blakes-button>
   </div>
   <h1>${accountResult?.display_name ?? `Blake Leonard`}</h1>
-  ${(accountResult?.note) ? /*html`<div class="p-4"><${SanitizedHTML} html=${accountResult.note} allowedTags=${tags} allowedClasses=${classes} /></div>`*/
-  html`<div class="p-4" dangerouslySetInnerHTML=${{__html: dompurifyBody(accountResult.note)}}><//>` : ``}
-  ${(accountResult?.fields) ? html`<div class="X-KVTable last:rounded-b-md overflow-clip">
+  ${/* (accountResult?.note) ? /*html`<div class="p-4"><${SanitizedHTML} html=${accountResult.note} allowedTags=${tags} allowedClasses=${classes} /></div>`* /
+  html`<div class="p-4" dangerouslySetInnerHTML=${{__html: dompurifyBody(accountResult.note)}}><//>` : */ ``}
+  <div class="p-4" name="About Me">
+    <p>A software developer with a passion for the powers, rights, and freedoms of users. Developer of dahliaOS, LucidLog, Bodacious, and more. Sometimes tries to design and write. 19 M (he/him) from Raleigh, NC. Cool tech enthusiast.</p>
+  </div>
+  ${/* (accountResult?.fields) ? html`<div class="X-KVTable last:rounded-b-md overflow-clip">
     ${(accountResult?.fields || []).map(field => html`<div class="row X-Uneven -last:border-b-2 -first:border-t-2 border-b-scheme-3" key=${field.name}>
       <span class="bg-scheme-3 font-semibold">${field.name}</span>
       <span class=${(field.verified_at ? "is-verified text-green-500" : "is-not-verified")+" X-Arb"}>
@@ -30,6 +33,6 @@ export default function ({ accountResult }: ({accountResult: Partial<Entity.Acco
         <span class="contents" dangerouslySetInnerHTML=${{__html: dompurifyBody(field.value)}}><//>
       </span>
     </div>`)}
-  </div>` : ``}
+  </div>` :*/ ``}
 </div>`;
 }
